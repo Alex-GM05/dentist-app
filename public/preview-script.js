04621c5de452fa88c731683a20a0bc7f44e9a311
Alex-GM <oalejandro.garcia.maciasipn@gmail.com>
2025-09-08 19:45:17 -0600

    Update preview-script.js

--- public/preview-script.js
+++ public/preview-script.js
@@ -226,7 +226,6 @@ function renderPreview(data) {
       <div>
         <div class="firma-line"></div>
         <p style="font-weight: bold; color: #2a9d8f;">Firma del odontólogo</p>
-        <p></p>
       </div>
     </div>
 
